6448e28f6fd17332e036da7668bf204c98bc409a
gu2rks <aa224iu@student.lnu.se>
2018-06-19 13:34:16 +0200

    Change.cpp now have two methods !! woop woop. im a f** boss

--- stageOne/adventureFive/Change.cpp
+++ stageOne/adventureFive/Change.cpp
@@ -2,11 +2,9 @@
 
 using namespace std;
 
-
-int main ()
+void core()
 {   
-    printf ("This program going to ask user Total(how much custumer need to pay) and Payment (how much custumer paid).\n");
-    printf ("The program going to give minimum number of Swedish bills and coins that should be returned rounded off to nearest krona \n");
+   
     //ask user
     int total, payment;
     cout << "Total (kr) :";
@@ -19,11 +17,19 @@ int main ()
         printf ("Payment is less then total amount \n");
         int change = payment - total;
         //abs(make negative digit to positive digit)
-        cout << "costumer need to pay" << abs(change) << " more. \nPls ask the custumer kindly :D" << endl;
-        main();
+        cout << "costumer need to pay " << abs(change) << " more. \nPls ask the custumer kindly :D" << endl;
+        core();
     }
 
     int change = payment - total;
     //1000 500 200 100 50 20 10 5 2 1
     cout << change << "kr \n";
 }
+
+int main(){
+    printf ("This program going to ask user Total(how much custumer need to pay) and Payment (how much custumer paid).\n");
+    printf ("The program going to give minimum number of Swedish bills and coins that should be returned rounded off to nearest krona \n");
+
+    core();
+}
+
